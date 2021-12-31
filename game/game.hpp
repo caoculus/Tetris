f811b031ce5b7b39f83431666a577b57314b52e9
jonathan <caojiasen@gmail.com>
2021-12-31 11:19:48 -0800

    added comments

--- game/game.hpp
+++ game/game.hpp
@@ -9,16 +9,33 @@ class tetris
 {
 
 public:
+    /**
+     * @brief Create a new instance of the tetris game.
+     * @param window the window where the game will be rendered
+     */
     explicit tetris(GLFWwindow *window) : keys_(window),
-                                 active_piece_(board_)
+                                          active_piece_(board_)
     {}
 
+    /**
+     * @brief Process one tick (frame) of the game.
+     * @throws game_over_exception if the game is over.
+     */
     void tick();
 
+    /**
+     * @return the number of frames that have elapsed since the game started.
+     */
     [[nodiscard]] int time() const noexcept;
 
+    /**
+     * @return the type of the next piece that will be spawned.
+     */
     [[nodiscard]] square next() const noexcept;
 
+    /**
+     * @return the contents of the game board.
+     */
     [[nodiscard]] const board_t &board() const noexcept;
 
 private:
@@ -31,22 +48,45 @@ private:
     bool line_clear_{false};
     piece active_piece_;
 
+    /**
+     * @brief Update the state of the game during the delay between pieces.
+     */
     void wait_delay();
 
+    /**
+     * @brief Collapse the cleared lines at the end of the line clear animation.
+     */
     void drop_lines();
 
+    /**
+     * @brief Attempt to spawn a new piece (with initial rotation based on the
+     * user input) if possible.
+     * @throws game_over_exception if the game is over
+     */
     void spawn_piece();
 
+    /**
+     * @brief Translate and rotate the game piece based on the user input.
+     */
     void move_piece();
 
+    /**
+     * @brief Draw the active piece onto the game board.
+     */
     void draw_piece();
 
+    /**
+     * @brief Check if there are any lines that can be cleared.
+     *
+     * @details Sets the line_clear_ flag if there are any and fills cleared
+     * rows with the type.
+     */
     void clear_lines();
 };
 
 class game_over_exception : public std::exception
 {
-    [[nodiscard]] constexpr const char * what() const noexcept override
+    [[nodiscard]] constexpr const char *what() const noexcept override
     {
         return "Game Over";
     }
