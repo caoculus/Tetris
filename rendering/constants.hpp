5d7ba9a5aab0bc2ed438d55cb4a1e91d42134ad2
jonathan <caojiasen@gmail.com>
2021-12-31 17:00:53 -0800

    constants change and board transformation

--- rendering/constants.hpp
+++ rendering/constants.hpp
@@ -1,5 +1,14 @@
 #pragma once
-#define F_LEFT (119.0lf / 320.0lf)
-#define F_RIGHT (199.0lf / 320.0lf)
+#define F_LEFT (120.0lf / 320.0lf)
+#define F_RIGHT (200.0lf / 320.0lf)
 #define F_TOP (40.0lf / 240.0lf)
 #define F_BOT (200.0lf / 240.0lf)
+
+/*
+ * Transformation for board:
+ * {
+ *     {1/20,  0,     -1/4 },
+ *     {0,     -1/15, 2/3  },
+ *     {0,     0,     1    }
+ * }
+ */
